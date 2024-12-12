35b0172e8954c81bf3f7cf49a3dce092c7efe0e0
Ahmed-Zakaria ESDAIRI <ahmedzakariaesdairi@gmail.com>
2024-12-12 23:41:19 +0100

    feat(Init) - Setup project

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+
+import bodyParser from 'body-parser';
+import catRoutes from './routes/cat.routes';
+import cors from 'cors';
+import express from 'express';
+ 
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/cats', catRoutes);
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to catmash-api!' });
+});
+
+
+// Launch the app on designated port
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
+
+export default app;
+
